e624b9d53eabf236c535f6170a9ffa63d9a0bd56
Berzki <mhdrizky32@gmail.com>
2026-03-10 14:22:37 +0700

    Initial Commit

---
+++ KawanSehat/KawanSehatApp.swift
@@ -0,0 +1,17 @@
+//
+//  KawanSehatApp.swift
+//  KawanSehat
+//
+//  Created by Muhammad Rizki on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KawanSehatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
